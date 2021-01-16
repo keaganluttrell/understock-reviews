01b39a6ada0c53422f1ccb7972591e7a92da3ff7
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-16 12:38:30 -0600

    filter function complete with clear button

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -3,49 +3,69 @@ import propTypes from 'prop-types';
 
 const Menu = ({ name, options, handler }) => {
   const [active, setActive] = useState(false);
-  const [current, setCurrent] = useState(options[0].head);
+  const [current, setCurrent] = useState(options[0]);
 
   return (
-    <div
-      id="mod-container"
-      onKeyDown={() => setActive(!active)}
-      onClick={() => setActive(!active)}
-      role="button"
-      tabIndex={0}
-    >
-      <fieldset>
-        <legend>{name}</legend>
-        <div className="fieldset-current">{current}</div>
-        <div className="r-menu-caret">
-          <i className={`fas fa-caret-${active ? 'up' : 'down'}`} />
-        </div>
-      </fieldset>
-
+    <>
       <div
-        id={`mod-menu-dropdown-${name}`}
-        style={{ display: active ? 'flex' : 'none' }}
+        id="mod-container"
+        onKeyDown={() => setActive(!active)}
+        onClick={() => setActive(!active)}
+        role="button"
+        tabIndex={0}
       >
-        {options.map((option, i) => {
-          const key = i;
-          return (
-            <div
-              className="fieldset-option"
-              onKeyDown={() => setCurrent(option.head)}
-              onClick={() => {
-                handler(option.body);
-                setCurrent(option.head);
-              }}
-              role="button"
-              tabIndex={0}
-              key={key}
-            >
-              {option.head}
+        <div>
+          <fieldset>
+            <legend>{name}</legend>
+            <div className="fieldset-current">{current.head}</div>
+            <div className="r-menu-caret">
+              <i className={`fas fa-caret-${active ? 'up' : 'down'}`} />
             </div>
-          );
-        })}
+          </fieldset>
+        </div>
 
+        <div
+          id={`mod-menu-dropdown-${name}`}
+          style={{ display: active ? 'flex' : 'none' }}
+        >
+          {options.map((option, i) => {
+            const key = i;
+            return (
+              <div
+                className="fieldset-option"
+                onKeyDown={() => setCurrent(option)}
+                onClick={() => {
+                  handler(option.body);
+                  setCurrent(option);
+                }}
+                role="button"
+                tabIndex={0}
+                key={key}
+              >
+                {option.head}
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <div
+        id="reviews-filter-clear-container"
+        style={{
+          display: current.body && name === 'Rating' ? 'flex' : 'none',
+        }}
+      >
+        <button
+          type="button"
+          id="reviews-filter-clear"
+          onClick={() => {
+            handler(0);
+            setCurrent(options[0]);
+          }}
+        >
+          Clear All
+        </button>
+      </div>
+    </>
   );
 };
 
