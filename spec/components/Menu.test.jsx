07fa925995fa77afe20331e0a735f91f9fdeda26
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-17 11:17:16 -0600

    menu tests complete

--- spec/components/Menu.test.jsx
+++ spec/components/Menu.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Menu from '../../client/components/Menu';
 import Star from '../../client/components/Star';
 
@@ -12,17 +12,59 @@ const filterOptions = [
   { head: <Star rating={1} />, body: 1 },
 ];
 
+const sortOptions = [
+  { head: <div>Most Recent</div>, body: 'review_date' },
+  { head: <div>Most Helpful</div>, body: 'helpful' },
+];
+
 describe('Filter Menu', () => {
+  const handler = jest.fn((x) => x);
   test('should pass an option to filter method in Reviews', () => {
     render(<Menu
       name="Rating"
       options={filterOptions}
-      handler={(value) => value}
+      handler={handler}
     />);
     const dropdown = document.getElementById('mod-container');
     fireEvent.click(dropdown);
-    const option = document.getElementById('RFF-all')
-    const val = fireEvent.click(option);
-    console.log(val)
+    const option = document.getElementsByClassName('fieldset-option')[4];
+    fireEvent.click(option);
+    expect(handler.mock.results[0].value).toBe(2)
+  });
+
+  test('should be able to clear all results', () => {
+    render(<Menu
+      name="Rating"
+      options={filterOptions}
+      handler={handler}
+    />);
+    const dropdown = document.getElementById('mod-container');
+    fireEvent.click(dropdown);
+    const option = document.getElementsByClassName('fieldset-option')[4];
+    fireEvent.click(option);
+    const clear = document.getElementById('reviews-filter-clear');
+    fireEvent.click(clear);
+    expect(handler.mock.results[2].value).toBe(0);
+  });
+});
+
+describe('Sort Menu', () => {
+  const handler = jest.fn((arg) => arg);
+  test('should pass an option to sort method in Reviews', () => {
+    render(<Menu
+      name="Rating"
+      options={sortOptions}
+      handler={handler}
+    />);
+    const dropdown = document.getElementById('mod-container');
+    fireEvent.click(dropdown);
+    const option = document.getElementsByClassName('fieldset-option');
+    fireEvent.click(option[1]);
+    fireEvent.click(dropdown);
+    fireEvent.click(option[0]);
+    expect(handler.mock.results[0].value).toBe('helpful');
+    expect(handler.mock.results[1].value).toBe('review_date');
+    fireEvent.keyDown(dropdown);
+    fireEvent.keyDown(option[1]);
   });
 });
