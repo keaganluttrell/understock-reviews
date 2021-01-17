b8dabe5d266a24cc94595fe6a848c1a283634485
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-17 01:12:15 -0600

    Review List Test suite complete

---
+++ spec/components/ReviewList.test.jsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import ReviewList from '../../client/components/ReviewList';
+import { data } from '../data/sampleReviews';
+
+describe('Review List', () => {
+  test('should display no reviews message when list is empty', () => {
+    render(<ReviewList list={[]} addHelpful={() => { }} />);
+    const text = document.getElementById('reviews-no-reviews').innerHTML
+    expect(text).not.toBe(undefined);
+  });
+
+  test('should display all reviews given in list', () => {
+    render(<ReviewList list={data} addHelpful={() => { }} />);
+    const reviews = document.getElementsByClassName('reviews-list-item');
+    expect(reviews.length).toBe(data.length);
+  });
+});
---
+++ spec/data/sampleReviews.js
@@ -0,0 +1,147 @@
+export const data = [
+  {
+    "helpful": 904,
+    "verified_purchase": false,
+    "images": [
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img6.jpg"
+    ],
+    "_id": "6000cf3d58202e16a48ec64e",
+    "product_id": 99,
+    "customer_name": "Dale F.",
+    "review_body": "Consectetur consectetur nulla non tempor laboris velit cillum aliquip.",
+    "review_title": "mediocre egrigious good kids??",
+    "rating": 4,
+    "review_date": "2020-09-15T05:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 19,
+    "verified_purchase": true,
+    "images": [
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img5.jpg"
+    ],
+    "_id": "6000cf3d58202e16a48ec65d",
+    "product_id": 99,
+    "customer_name": "Minnie H.",
+    "review_body": "Magna sunt labore minim commodo dolore. Cupidatat dolore est nostrud fugiat mollit. Amet Lorem elit sit quis veniam in ipsum aliqua excepteur consequat. Anim ut esse nostrud do laborum enim laboris. Cupidatat qui ad qui officia. Enim est nostrud sint exercitation consequat est elit deserunt.",
+    "review_title": "too even though????",
+    "rating": 3,
+    "review_date": "2020-12-07T06:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 8,
+    "verified_purchase": true,
+    "images": [
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img22.jpg",
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img14.jpg",
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img29.jpg"
+    ],
+    "_id": "6000cf3d58202e16a48ec65a",
+    "product_id": 99,
+    "customer_name": "Simon V.",
+    "review_body": "Dolor est non ex ea quis esse dolore labore adipisicing. Occaecat ullamco est aute ut sunt dolor eu ipsum enim. Labore ea voluptate pariatur incididunt Lorem fugiat.",
+    "review_title": "mediocre way",
+    "rating": 5,
+    "review_date": "2020-06-11T05:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 4,
+    "verified_purchase": true,
+    "images": [],
+    "_id": "6000cf3d58202e16a48ec655",
+    "product_id": 99,
+    "customer_name": "Ellen C.",
+    "review_body": "Fugiat duis quis labore consectetur esse officia Lorem dolor. Ipsum ad sit consectetur id velit sunt duis.",
+    "review_title": "awesome expected",
+    "rating": 1,
+    "review_date": "2020-01-20T06:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 3,
+    "verified_purchase": false,
+    "images": [
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img4.jpg",
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img11.jpg"
+    ],
+    "_id": "6000cf3d58202e16a48ec658",
+    "product_id": 99,
+    "customer_name": "Leon O.",
+    "review_body": "Ut officia et proident reprehenderit laboris aliqua ea sit laborum. Pariatur est incididunt reprehenderit sit veniam quis deserunt ut. Id amet commodo proident enim minim id amet exercitation laboris ad sit. Exercitation ea culpa aute cupidatat. Velit sit eu excepteur sit incididunt ut.",
+    "review_title": "man top-notch even though for the",
+    "rating": 3,
+    "review_date": "2020-03-05T06:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 3,
+    "verified_purchase": true,
+    "images": [],
+    "_id": "6000cf3d58202e16a48ec657",
+    "product_id": 99,
+    "customer_name": "Bill C.",
+    "review_body": "Mollit enim voluptate ex id Lorem minim.",
+    "review_title": "way kids wife",
+    "rating": 5,
+    "review_date": "2019-01-28T06:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 2,
+    "verified_purchase": true,
+    "images": [],
+    "_id": "6000cf3d58202e16a48ec668",
+    "product_id": 99,
+    "customer_name": "Minnie I.",
+    "review_body": "Labore aliqua ut ut labore Lorem eu dolore. Magna excepteur culpa voluptate amet eiusmod id.",
+    "review_title": "unbelievable bad house less than egrigious",
+    "rating": 2,
+    "review_date": "2019-03-25T05:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 2,
+    "verified_purchase": false,
+    "images": [
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img27.jpg",
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img17.jpg",
+      "https://fec-aws-images.s3.us-east-2.amazonaws.com/images/img12.jpg"
+    ],
+    "_id": "6000cf3d58202e16a48ec65b",
+    "product_id": 99,
+    "customer_name": "Sonny V.",
+    "review_body": "Nostrud deserunt aliquip dolor anim tempor est esse minim veniam veniam quis.",
+    "review_title": "egrigious  crap egrigious",
+    "rating": 3,
+    "review_date": "2018-09-23T05:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 1,
+    "verified_purchase": true,
+    "images": [],
+    "_id": "6000cf3d58202e16a48ec650",
+    "product_id": 99,
+    "customer_name": "Evan Q.",
+    "review_body": "Culpa aliquip duis enim in.",
+    "review_title": "could have been par cheddar for the",
+    "rating": 1,
+    "review_date": "2017-10-25T05:00:00.000Z",
+    "__v": 0
+  },
+  {
+    "helpful": 1,
+    "verified_purchase": false,
+    "images": [],
+    "_id": "6000cf3d58202e16a48ec656",
+    "product_id": 99,
+    "customer_name": "Pedro K.",
+    "review_body": "Eu voluptate cillum irure velit non labore quis eiusmod ex.",
+    "review_title": "top-notch over my  trash????",
+    "rating": 4,
+    "review_date": "2017-03-27T05:00:00.000Z",
+    "__v": 0
+  }
+];
