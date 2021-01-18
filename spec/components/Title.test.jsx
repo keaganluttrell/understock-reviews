1abe5738c885b84f591f790d91a8b1466729f305
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-18 17:19:45 -0600

    title component tested

--- spec/components/Title.test.jsx
+++ spec/components/Title.test.jsx
@@ -9,6 +9,31 @@ describe('Title', () => {
   const mockDispay = jest.fn(x => x);
 
 
-  render(<Title rating={4.5} display={false} setDisplay={mockDispay} />);
-  test('Should properly render', () =)
-})
+  test('Should toggle content display', () => {
+    render(<Title rating={4.5} display={false} setDisplay={mockDispay} />);
+    const title = document.getElementById('reviews-title');
+    fireEvent.click(title);
+    fireEvent.click(title);
+    expect(mockDispay.mock.results[0].value).toBe.Truthy;
+    expect(mockDispay.mock.results[1].value).not.toBe.Truthy;
+    fireEvent.keyDown(title);
+    fireEvent.keyDown(title);
+    expect(mockDispay.mock.results[2].value).toBe.Truthy;
+    expect(mockDispay.mock.results[3].value).not.toBe.Truthy;
+  });
+
+  test('Should display stats in header if display is false', () => {
+    render(<Title rating={4.5} display={false} setDisplay={mockDispay} />);
+    const stats = document.querySelector('#reviews-title-stats');
+    let display = stats.getAttribute('style');
+    expect(display).toBe('display: flex;');
+  });
+
+  test('Should not display stats in header if display is true', () => {
+    render(<Title rating={4.5} display={true} setDisplay={mockDispay} />);
+    const stats = document.querySelector('#reviews-title-stats');
+    let display = stats.getAttribute('style');
+    expect(display).toBe('display: none;');
+  });
+
+});
