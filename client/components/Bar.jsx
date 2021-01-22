af421f39cee673512141d1ec600c85f1c1b82011
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-21 21:55:41 -0600

    quick refactor on bar display

--- client/components/Bar.jsx
+++ client/components/Bar.jsx
@@ -11,12 +11,8 @@ const Bar = ({
   <div className="RGB-bar-group">
     <div className="RGB-bar-bottom">
       <div
-        className="RGB-bar-top"
-        style={{
-          width: `${avg * 100}%`,
-          backgroundColor: rating === filter
-            ? 'rgb(244, 186, 49)' : 'rgba(0, 0, 0, 0.70)',
-        }}
+        className={`RGB-bar-top${rating === filter ? '-selected' : ''}`}
+        style={{ width: `${avg * 100}%` }}
       />
     </div>
     <Star rating={rating} />
