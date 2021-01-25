b81fc7ce8083aa7fe651e351e7242137b1348481
Keagan Luttrell <keaganluttrell7@gmail.com>
2021-01-25 16:12:47 -0600

    script change, console msg for node server

--- server/server.js
+++ server/server.js
@@ -51,4 +51,4 @@ app.patch('/:product_id/reviews/:id', (req, res) => {
     .then((review) => res.send(review));
 });
 
-app.listen(PORT);
+app.listen(PORT, console.log('Reviews Service running on', PORT));
